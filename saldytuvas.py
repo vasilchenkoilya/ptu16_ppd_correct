5f22e52545b362ce3fafcc7c26092cb27c430ba8
vasilchenkoilya <ilya.vas@outlook.com>
2023-08-18 15:21:37 +0300

    programos sablonas

---
+++ saldytuvas.py
@@ -0,0 +1,35 @@
+
+# while True : - True
+# 1 - pridėti naują produktą - Ilya
+
+# 2 - papildyti produkto kiekį - Igoris
+
+# 3 - ištraukti produktą nurodant kiekį -Eimantas
+
+# 4 - peržiūrėti produktus - Einaras
+
+# 5 - ieškoti produktų - Arnoldas
+
+# 0 - išėjimas - Ilya
+saldytuvas = {}
+meniu = """
+ 1 - pridėti naują produktą 
+ 2 - papildyti produkto kiekį
+ 3 - ištraukti produktą nurodant kiekį
+ 4 - peržiūrėti produktus
+ 5 - ieškoti produktų 
+ 0 - išėjimas  """
+while True:
+    pasirinkimas = input("Pasirinkite: ")
+    if pasirinkimas == "0":
+        break
+    elif pasirinkimas == "1":
+        pass
+    elif pasirinkimas == "2":
+        pass
+    elif pasirinkimas == "3":
+        pass
+    elif pasirinkimas == "4":
+        pass
+    elif pasirinkimas == "5":
+        pass
