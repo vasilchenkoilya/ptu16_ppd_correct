6f5d76476c78e7fe3e6afe66d4e7a36d0796182f
Eimantasj1 <eimantas.jaseviciuss@gmail.com>
2023-08-28 15:47:50 +0300

    def istraukti

--- sadytuvas_2.py
+++ sadytuvas_2.py
@@ -9,24 +9,35 @@ except:
 class Saldytuvas:
     turinys = saldytuvas
 
-    def prideti(self, product:str, quantity:float):
-        self.turinys[product] = quantity
+def prideti(self, product:str, quantity:float):
+    self.turinys[product] = quantity
 
-    def papildyti(self, product:str, quantity:float):
-        pass
+def papildyti(self, product:str, quantity:float):
+    pass
     
-    def istraukti(self, product:str, quantity:float):
-        pass
+def istraukti(self, product:str):
+    if product in self.turinys:
+        kiekis = float(input(f"Įveskite kiekį, kurį norite ištraukti (turimas kiekis: {self.turinys[product]}): "))
+        if kiekis <= self.turinys[product]:
+            self.turinys[product] -= kiekis
+            print(f"{product} ištraukta {kiekis}, Dabartinis kiekis: {self.turinys[product]}")
+            if self.turinys[product] == 0:
+                del self.turinys[product]
+        else:
+            print('Nepakankamas kiekis šaldytuve.')
+    else:
+        print(f"Produktas {product} nerastas šaldytuve.")
+    pass
 
-    def perziureti(self):
-        pass
+def perziureti(self):
+    pass
 
-    def ieskoti_produkta(self, product:str):
-        pass
+def ieskoti_produkta(self, product:str):
+    pass
 
-    def svoris(self):
-        pass
+def svoris(self):
+    pass
 
-    def receptas(self, product:str, quantity:float):
-        pass
+def receptas(self, product:str, quantity:float):
+    pass
     
