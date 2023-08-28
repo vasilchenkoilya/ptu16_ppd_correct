5404d6ecf6c21cf5534e4dbf300d5aa3c4810dea
Igor <nikitinigorra@gmail.com>
2023-08-28 15:09:47 +0300

    papildyti funkcija atnaujinta kaip objektas

--- sadytuvas_2.py
+++ sadytuvas_2.py
@@ -12,8 +12,26 @@ class Saldytuvas:
     def prideti(self, product:str, quantity:float):
         self.turinys[product] = quantity
 
-    def papildyti(self, product:str, quantity:float):
-        pass
+    def papildyti(self) -> str | float :
+        """Funkcija isves menu, kur bus parodyta lentele 
+        su visais maisto produktais ir jiems priskirti 
+        eiles numeriai
+        """
+        indeksas = 0
+
+        print("Saldytuve yra tokie produktai: ", "\n")
+        print(f"{'Nr.':3s} | {'Maisto produktas':15s} | {'Produkto kiekis':10s}", end="\n")
+        for produktas in self:
+            print(f"{indeksas+1:>3d} | {produktas:<16s} | {self[produktas]}")
+            indeksas += 1
+
+        pasirinktas_indeksas = int(input("Parasykite norimo produkto numeri: ")) -1
+        prideti = int(input("Parasykite kiek norite prideti produkto: "))
+
+        pasirinktas_produktas = self[pasirinktas_indeksas]
+        self[pasirinktas_produktas] += prideti
+
+        return self
     
     def istraukti(self, product:str, quantity:float):
         pass
